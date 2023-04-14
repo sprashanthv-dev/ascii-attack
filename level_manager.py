c5928c003d7e2b0c07d498ff93cbcf2697e66047
ashwin <adithyaviswam@gmail.com>
2023-04-14 17:55:33 -0600

    aligning welcome screen

--- level_manager.py
+++ level_manager.py
@@ -1,2 +1,21 @@
 class LevelManager:
-  pass
+    def __init__(self):
+        self.level = 0
+        self.blocks_left = 0
+        self.misses_left = 0
+
+    def load_level(self, level: int):
+        # Placeholder for loading level logic
+        pass
+
+    def setup_level_ui(self):
+        # Placeholder for setting up level UI logic
+        pass
+
+    def handle_key_stroke(self):
+        # Placeholder for handling key stroke logic
+        pass
+
+    def handle_missed_block(self):
+        # Placeholder for handling missed block logic
+        pass
--- welcome_screen.py
+++ welcome_screen.py
@@ -5,24 +5,29 @@ from pygame.locals import *
 class WelcomeScreen:
     def __init__(self, game_manager):    
         self.game_manager = game_manager
-        
         self.screen = self.game_manager.screen
         self.title = self.game_manager.title
         self.width = self.game_manager.width
         self.height = self.game_manager.height
         
-        self.background_image = pygame.image.load("./assets/img/main_menu.png")
-        
+        self.background_image = pygame.image.load("./assets/img/main_menu2.png")
+        self.background_image = pygame.transform.scale(self.background_image, (self.width, self.height))
+
         self.game_manager.play_background_music()
         
         self.main_menu_title = "Main Menu"
+        self.font = pygame.font.Font("./assets/fonts/NiceSugar.ttf", 64)
         
-        font = pygame.font.Font("./assets/fonts/NiceSugar.ttf", 64)
+        self.button_spacing = 80
+        self.button_width = 200
+        self.button_height = 50
+        self.button_x = self.width/2 - self.button_width/2
+        self.button_y = self.height/2 - self.button_height/2
         
-        self.start_game = Button(self.screen, "New Game", 448, 200, font)
-        self.view_rules = Button(self.screen, "Rules", 448, 250, font)
-        self.view_leaderboard = Button(self.screen, "Leaderboard", 448, 300, font)
-        self.quit_game = Button(self.screen, "Quit", 448, 350, font)
+        self.start_game = Button(self.screen, "New Game", self.button_x, self.button_y, self.font)
+        self.view_rules = Button(self.screen, "Rules", self.button_x, self.button_y + self.button_spacing, self.font)
+        self.view_leaderboard = Button(self.screen, "Leaderboard", self.button_x, self.button_y + 2*self.button_spacing, self.font)
+        self.quit_game = Button(self.screen, "Quit", self.button_x, self.button_y + 3*self.button_spacing, self.font)
         
     def handle_interactions(self):
         mouse_pos = pygame.mouse.get_pos()
@@ -49,7 +54,7 @@ class WelcomeScreen:
         
         font = pygame.font.Font("./assets/fonts/SuperMario256.ttf", 96)
         title_text = font.render(self.title, True, (255, 201, 60))
-        self.screen.blit(title_text, (150, 100))
+        self.screen.blit(title_text, (150, 180))
         
         font = pygame.font.Font("./assets/fonts/NiceSugar.ttf", 64)
         main_menu_text = font.render(self.main_menu_title, True, (255, 255, 255))
